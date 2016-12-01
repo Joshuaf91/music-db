dc3a249e3e11cc69692c3af3573a3bd89bd754d8
Joshua Fermin <Joshuaf91@gmail.com>
2016-12-01 16:53:13 -0500

    created playlist_model

--- models/playlist-model.js
+++ models/playlist-model.js
@@ -1,8 +1,21 @@
 const Sequelize = require('sequelize');
 const sequelizeConnection = require('../db');
+const Song = require('./song-model');
 
 //////////
 // YOUR CODE HERE:
 //////////
+
+var Playlist = sequelizeConnection.define('playlist', {
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+    	len: [1,100]
+    }
+  }
+});
+
+Playlist.belongsToMany(Song,{through:PlaylistSong});
+Song.belongsToMany(Playlist,{through:PlaylistSong});
 
 module.exports = Playlist;
